a5aaa61fe6f068aa3fba040f8b2829eda68cd340
InnovatorZhang <zhangzhonghao1997@gmail.com>
2022-04-07 22:14:47 +0800

    添加代码中文注释

--- VirtualScreen.cpp
+++ VirtualScreen.cpp
@@ -16,29 +16,29 @@ void VirtualScreen::create(unsigned int w, unsigned int h, float pixel_size, sf:
             auto index = (x * m_screenSize.y + y) * 6;
             sf::Vector2f coord2d(x * m_pixelSize, y * m_pixelSize);
 
-            //Triangle-1
-            //top-left
+            // 第一个三角形
+            // 左上角
             m_vertices[index].position = coord2d;
             m_vertices[index].color = color;
 
-            //top-right
+            // 右上角
             m_vertices[index + 1].position = coord2d + sf::Vector2f{ m_pixelSize, 0 };
             m_vertices[index + 1].color = color;
 
-            //bottom-right
+            // 右下角
             m_vertices[index + 2].position = coord2d + sf::Vector2f{ m_pixelSize, m_pixelSize };
             m_vertices[index + 2].color = color;
 
-            //Triangle-2
-            //bottom-right
+            // 第二个三角形
+            // 右下角
             m_vertices[index + 3].position = coord2d + sf::Vector2f{ m_pixelSize, m_pixelSize };
             m_vertices[index + 3].color = color;
 
-            //bottom-left
+            // 左下角
             m_vertices[index + 4].position = coord2d + sf::Vector2f{ 0, m_pixelSize };
             m_vertices[index + 4].color = color;
 
-            //top-left
+            // 左上角
             m_vertices[index + 5].position = coord2d;
             m_vertices[index + 5].color = color;
         }
@@ -47,9 +47,9 @@ void VirtualScreen::create(unsigned int w, unsigned int h, float pixel_size, sf:
 
 /*
 * 设置每个像素点的颜色，其实每个像素由两个三角形组成，如下所示
-* 1112
-* 1122
-* 1222
+* 2111
+* 2211
+* 2221
 */
 void VirtualScreen::setPixel(std::size_t x, std::size_t y, sf::Color color)
 {
--- VirtualScreen.h
+++ VirtualScreen.h
@@ -4,15 +4,18 @@
 class VirtualScreen : public sf::Drawable
 {
 public:
+    // 创建每个顶点的位置以及初始值
     void create(unsigned int width, unsigned int height, float pixel_size, sf::Color color);
+    // 设置每一个点的像素值，每一次一个像素，由两个三角形，6个顶点组成，所以在该方法处赋值像素对应的6个顶点
     void setPixel(std::size_t x, std::size_t y, sf::Color color);
 
 private:
+    // 必须要复写该方法，定义了如何画出自身
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
-    sf::Vector2u m_screenSize;
+    sf::Vector2u m_screenSize;  // 屏幕大小，nes是256x240
     float m_pixelSize; //virtual pixel size in real pixels
-    sf::VertexArray m_vertices;
+    sf::VertexArray m_vertices;  // 顶点数组， m_vertices.setPrimitiveType(sf::Triangles)，表示三个顶点一组，画三角形，由三角形组成像素
 
 public:
     // 定义NES的屏幕大小
